657b94083c33fa74a4021c118d59f974f2b5ed7d
Simon Wibmer <simon.wibmer@gmx.at>
2020-06-12 09:29:12 +0200

    yok

--- server.py
+++ server.py
@@ -7,8 +7,16 @@ def index():
     return 'Hello world'
 
 @app.route('/eller')
-def index():
+def index2():
     return 'Schmutzzulage im Wert von 5k€'
+
+
+
+
+
+@app.route('/yok')
+def yok():
+    return 'yok'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
