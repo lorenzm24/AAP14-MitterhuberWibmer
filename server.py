f7d05015f95520cacb719b68be7a30640338073b
Lorenz Mitterhuber <mitterhuber.lorenz@gmail.com>
2020-06-12 09:15:33 +0200

    /eller hinzugefügt

--- server.py
+++ server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return 'Hello world'
 
+@app.route('/eller')
+def index():
+    return 'Schmutzzulage im Wert von 5k€'
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
