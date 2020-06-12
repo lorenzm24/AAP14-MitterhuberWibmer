c9cc07bcdc21ffdc8f69deb655be17f11f0a109b
Simon Wibmer <simon.wibmer@gmx.at>
2020-06-12 09:10:34 +0200

    Von Website eini kopiert

--- server.py
+++ server.py
@@ -1,1 +1,10 @@
-print("Hallo")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello world'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
