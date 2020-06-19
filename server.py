23e1c7bd5aa9e52f19b353689b50a4e9751c7232
lorenzm24 <mitterhuber.lorenz@gmail.com>
2020-06-19 13:04:41 +0200

    Es wurde Änderungen in der Struktur, Design, etc. vorgenommen

--- server.py
+++ server.py
@@ -1,14 +1,30 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+news = [
+    {
+        'author': 'Alfred Stumfpel',
+        'title': 'Schlagezeile 1',
+        'content': 'DIC',
+        'date_posted': '1. Juni 2020'
+    },
+    {
+        'author': 'Eller Johannes',
+        'title': 'Schlagezeile 2',
+        'content': 'Schmutzzulage',
+        'date_posted': '10. Juni 2020'
+    }
+]
+
+
 @app.route('/')
-def index():
-    return 'Hello world'
+@app.route('/home')
+def home():
+    return render_template('home.html', news=news)
 
 @app.route('/eller')
-def index2():
-    return render_template('index_eller.html')
+def eller():
+    return render_template('eller.html', title='Eller')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
